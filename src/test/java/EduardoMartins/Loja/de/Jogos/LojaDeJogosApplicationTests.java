847a992d12b92c5c2a66359d5aca2d1982b7c13d
eduardosackser07 <martinseduardo261@gmail.com>
2025-03-17 21:16:11 -0300

    commit dos arquivos iniciais

---
+++ src/test/java/EduardoMartins/Loja/de/Jogos/LojaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package EduardoMartins.Loja.de.Jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
